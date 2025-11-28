618c70ce27aa5d15cdfa05381cd5a27d4c672bab
ahmadfurrokh29 <ahmadfurrokh@gmail.com>
2025-11-28 16:33:49 +0500

    updated the main file and added a new file

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
       
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Github demos',
       
 
       home: const Task1(),
